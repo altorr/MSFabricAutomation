0b06f24922439865c404216650a62cadcc8bc54f
altorr <50384724+altorr@users.noreply.github.com>
2025-05-21 22:12:46 -0500

    Update SuspendCapacity.ps1

--- SuspendCapacity.ps1
+++ SuspendCapacity.ps1
@@ -2,6 +2,6 @@ $AzureConnection = (Connect-AzAccount -Identity).context
 
 Set-AzContext -SubscriptionName $AzureConnection.Subscription -DefaultProfile $AzureConnection
 
-Suspend-AzFabricCapacity -ResourceGroupName "rgfabrictest" -CapacityName "demofabrictest2"
+Suspend-AzFabricCapacity -ResourceGroupName "yourResourceGroup" -CapacityName "yourFabricService"
 
 write-output "Suspend executed"
