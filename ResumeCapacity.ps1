bd311021cd3216fbffd060cbfe8f797412d2d3dc
altorr <50384724+altorr@users.noreply.github.com>
2025-05-21 22:12:20 -0500

    Update ResumeCapacity.ps1

--- ResumeCapacity.ps1
+++ ResumeCapacity.ps1
@@ -2,6 +2,6 @@ $AzureConnection = (Connect-AzAccount -Identity).context
 
 Set-AzContext -SubscriptionName $AzureConnection.Subscription -DefaultProfile $AzureConnection
 
-Resume-AzFabricCapacity -ResourceGroupName "rgfabrictest" -CapacityName "demofabrictest2"
+Resume-AzFabricCapacity -ResourceGroupName "yourResourceGroup" -CapacityName "yourFabricService"
 
 write-output "Resume executed"
